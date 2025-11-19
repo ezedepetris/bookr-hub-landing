8b8b5768d1d88b7036472f1317b1e49ac82e1d46
Ezequiel Depetris <ezedepetris@gmail.com>
2025-11-19 11:35:41 -0300

    Sitemap

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'net/http'
 require 'json'
+require 'date'
 require 'stripe'
 require 'dotenv/load'
 require 'i18n'
@@ -103,7 +104,6 @@ before do
 end
 
 get '/' do
-  puts "\n\n\n\n\n\n\n\n\nSession: #{session.inspect}\n\n\n\n\n\n\n\n\n"
   @prices = StripeService.get_stripe_prices(session[:currency])
   @locale_currency_map = LOCALE_CURRENCY_MAP
   erb :index
@@ -115,6 +115,68 @@ end
 
 get '/oops' do
   erb :oops
+end
+
+get '/robots.txt' do
+  content_type 'text/plain'
+  base_url = I18n.t('site.canonical_url', locale: :en)
+  "User-agent: *\nAllow: /\n\nSitemap: #{base_url}/sitemap.xml"
+end
+
+get '/sitemap.xml' do
+  content_type 'application/xml; charset=utf-8'
+  headers 'Cache-Control' => 'public, max-age=3600'
+
+  base_url = I18n.t('site.canonical_url', locale: :en)
+  today = Date.today.strftime('%Y-%m-%d')
+
+  # Get all available locales
+  available_locales = I18n.available_locales
+
+  xml = ['<?xml version="1.0" encoding="UTF-8"?>']
+  xml << '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"'
+  xml << '        xmlns:xhtml="http://www.w3.org/1999/xhtml">'
+
+  # Home page with all locale variations
+  available_locales.each do |locale|
+    locale_str = locale.to_s
+    currency = LOCALE_CURRENCY_MAP[locale_str] || LOCALE_CURRENCY_MAP[locale_str.split('-').first] || 'USD'
+    url = "#{base_url}/?locale=#{locale_str}&currency=#{currency}"
+    # Escape ampersands for XML
+    url_escaped = url.gsub('&', '&amp;')
+
+    xml << '  <url>'
+    xml << "    <loc>#{url_escaped}</loc>"
+    xml << "    <lastmod>#{today}</lastmod>"
+    xml << '    <changefreq>weekly</changefreq>'
+    xml << '    <priority>1.0</priority>'
+
+    # Add alternate language links
+    available_locales.each do |alt_locale|
+      alt_locale_str = alt_locale.to_s
+      alt_currency = LOCALE_CURRENCY_MAP[alt_locale_str] || LOCALE_CURRENCY_MAP[alt_locale_str.split('-').first] || 'USD'
+      alt_url = "#{base_url}/?locale=#{alt_locale_str}&currency=#{alt_currency}"
+      alt_url_escaped = alt_url.gsub('&', '&amp;')
+      xml << "    <xhtml:link rel=\"alternate\" hreflang=\"#{alt_locale_str}\" href=\"#{alt_url_escaped}\"/>"
+    end
+    default_url = "#{base_url}/?locale=en&currency=USD"
+    default_url_escaped = default_url.gsub('&', '&amp;')
+    xml << "    <xhtml:link rel=\"alternate\" hreflang=\"x-default\" href=\"#{default_url_escaped}\"/>"
+
+    xml << '  </url>'
+  end
+
+  # Privacy page
+  xml << '  <url>'
+  xml << "    <loc>#{base_url}/privacy</loc>"
+  xml << "    <lastmod>#{today}</lastmod>"
+  xml << '    <changefreq>monthly</changefreq>'
+  xml << '    <priority>0.5</priority>'
+  xml << '  </url>'
+
+  xml << '</urlset>'
+
+  xml.join("\n")
 end
 
 post '/set_locale' do
